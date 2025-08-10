475ce3a2cec76487763c3d8dd98d14192492810f
SefiatAhmedAmine <amine-sefiat@hotmail.com>
2025-08-10 21:06:20 +0100

    01-Lombok-POJOs

---
+++ src/main/java/com/aas/spring6restmvc/models/Car.java
@@ -0,0 +1,19 @@
+package com.aas.spring6restmvc.models;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+public class Car {
+    private UUID id;
+    private Integer year;
+    private String carName;
+    private CarStyle carStyle;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+}
---
+++ src/main/java/com/aas/spring6restmvc/models/CarStyle.java
@@ -0,0 +1,5 @@
+package com.aas.spring6restmvc.models;
+
+public enum CarStyle {
+    SUV, SPORTS_CAR, CONVERTIBLE, MINIVAN, SEDAN, PICKUP, LIMOUSINE
+}
