fc6a9c46b34b5a181af4cee331fdd10c8bcc79cc
SefiatAhmedAmine <amine-sefiat@hotmail.com>
2025-08-10 23:48:38 +0100

    06-mvc-get-with-path-variable

--- src/main/java/com/aas/spring6restmvc/controllers/CarController.java
+++ src/main/java/com/aas/spring6restmvc/controllers/CarController.java
@@ -4,7 +4,9 @@ import com.aas.spring6restmvc.models.Car;
 import com.aas.spring6restmvc.services.CarService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,15 +15,17 @@ import java.util.UUID;
 @RestController
 @AllArgsConstructor
 @Slf4j
+@RequestMapping("/api/v1/cars")
 public class CarController {
     private final CarService carService;
 
-    @RequestMapping("/api/v1/cars")
+    @RequestMapping(method = RequestMethod.GET)
     public List<Car> getCars() {
         return carService.getCars();
     }
 
-    public Car getCarById(UUID carId) {
+    @RequestMapping(value = "/{carId}", method =  RequestMethod.GET)
+    public Car getCarById(@PathVariable("carId") UUID carId) {
         log.debug("CarServiceImpl.getCarById was called with carId={}", carId);
         return carService.getCarById(carId);
     }
